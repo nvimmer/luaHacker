e6ddb90de51d7c6da3b760c7cc3ab58aed88ac50
guoliang <tswanggl@gmail.com>
2022-08-04 00:06:36 +0800

    nil and Boolean test
    false : false and nil
    true  : others

--- chap1/type.lua
+++ chap1/type.lua
@@ -12,10 +12,15 @@ print(type(nil))
 -- Conditional tests consider both the Boolean false and nil as false and
 -- anything else true. In particular, Lua considers both zero and the empty
 -- string as true in conditional tests.
+
 print(type(true))
+
+-- false when boolean type "false" and "nil"
+
 local empty = false
 local False = nil
-if not False and not False then
+
+if not empty and not False then
 	print("empty and False are false")
 end
 
@@ -26,6 +31,35 @@ if Zero and Str then
 	print("Zero(0) and Str(\"\") are true")
 end
 
+-- and: The first operation when the first is false
+--      return the second value when the first is true
+4 and 5                -- 5
+nil and 13             -- nil
+false and 13           -- false
+
+-- or: return the first value when the first operation is not false
+
+0 or 5                 -- 0
+false or "hi"          -- "hi"
+nil or false           -- false
+
+
+
+x = x or v
+
+if not x then
+  x = v
+end
+
+((a and b) or c)
+(a and b or c)
+
+-- not always return Boolean type value
+not nil              -- true
+not false            -- true
+not 0                -- false
+not not 1            -- true
+not not nil          -- false
 
 print(type(10.5 * 4))
 print(type("Hello world"))
@@ -33,3 +67,4 @@ print(type(io.stdin))
 print(type(print))
 print(type(type))
 print(type({}))
+print(type(type(X)))
