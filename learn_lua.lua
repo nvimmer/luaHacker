d8f495c082c37cac2567121e61e47f08f1d3cc14
guoliang <tswanggl@gmail.com>
2022-08-03 14:02:57 +0800

    dic

--- learn_lua.lua
+++ learn_lua.lua
@@ -142,4 +142,19 @@ for _, value in ipairs(colors) do
 end
 
 
+local info ={
+  name = "sid",
+  age = 20,
+  isAlive = true
+}
+
+print(info[name])
+print(info.name)
+
+for key, value in pairs(info) do
+  print(key .. " " .. value)
+end
+
+
+
 
