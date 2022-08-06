b399340b8ac7fdf6f89a30b11e8da30862cbbf47
guoliang <tswanggl@gmail.com>
2022-08-06 19:15:48 +0800

    alg opration

--- chap1/number.lua
+++ chap1/number.lua
@@ -6,3 +6,31 @@ print(math.type(3.0))
 local x = 0xa.bp2
 print(x)
 print(string.format("%a",0.1))
+
+-- +
+print(1.+2)
+
+-- - 
+print(2 - 1)
+
+--  *
+print(2* 2.0)
+
+-- /
+
+print(3 / 2)
+
+-- //
+
+print(3 // 2)
+
+-- %
+
+print( 3 % 2)
+print(math.pi -  x % 0.01)
+print(math.pi -  x % 0.001)
+
+-- ^
+
+print(2^3)
+
