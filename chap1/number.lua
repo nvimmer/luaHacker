b268f46a6c3c8f4ad03526088dc5e2ae04edf292
guoliang <tswanggl@gmail.com>
2022-08-06 19:29:24 +0800

    unbiased rounding
    round

--- chap1/number.lua
+++ chap1/number.lua
@@ -53,6 +53,25 @@ print(math.random(1, 60))
 print(math.randomseed(os.time()))
 
 
+print(math.floor(3.3))
+print(math.floor(-3.3))
+print(math.ceil(3.3))
+print(math.ceil(-3.3))
+print(math.modf(3.3))
+print(math.modf(-3.3))
 
 
 
+local function round(x)
+  local f = math.floor(x)
+  if(x == f) or ( x % 2.0 == 0.5)  then
+    return f
+  else
+    return math.floor(x + 0.5)
+  end
+end
+
+print(round(2.5))
+print(round(3.5))
+print(round(-2.5))
+print(round(-1.5))
